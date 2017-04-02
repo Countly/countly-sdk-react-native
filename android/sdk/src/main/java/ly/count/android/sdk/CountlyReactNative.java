8e102a4471f122c0fa1e040dae6c6415a07ff5ba
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2017-04-02 19:13:01 +0530

    android file updated to cordova

--- android/sdk/src/main/java/ly/count/android/sdk/CountlyReactNative.java
+++ android/sdk/src/main/java/ly/count/android/sdk/CountlyReactNative.java
@@ -56,99 +56,254 @@ public class CountlyReactNative extends ReactContextBaseJavaModule {
 
 	@ReactMethod
 	public void init(ReadableArray args){
-		String serverUrl = args.getString(0);
+        String serverUrl = args.getString(0);
         String appKey = args.getString(1);
-		Countly.sharedInstance().init(_reactContext, serverUrl, appKey);
+
+        if(args.length() == 2){
+            Countly.sharedInstance()
+                .init(context, serverUrl, appKey,null,DeviceId.Type.OPEN_UDID);
+        }else if(args.length() == 3){
+            String yourDeviceID = args.getString(2);
+            Countly.sharedInstance()
+                .init(context, serverUrl, appKey,yourDeviceID,null);
+        }else{
+            Countly.sharedInstance()
+                .init(context, serverUrl, appKey,null,DeviceId.Type.ADVERTISING_ID);
+        }
 		Countly.sharedInstance().onStart(getCurrentActivity());
 	}
 
+    @ReactMethod
+    public void changeDeviceId(ReadableArray args){
+        String newDeviceID = args.getString(0);
+        Countly.sharedInstance().changeDeviceId(newDeviceID);  
+    }
+    
+    @ReactMethod
+    public void setHttpPostForced(ReadableArray args){
+        int isEnabled = Integer.parseInt(args.getString(0));
+        if(isEnabled == 1){
+            Countly.sharedInstance().setHttpPostForced(true);
+        }else{
+            Countly.sharedInstance().setHttpPostForced(false);
+        }
+    }
+    
+    @ReactMethod
+    public void enableParameterTamperingProtection(ReadableArray args){
+        String salt = args.getString(0);
+        Countly.sharedInstance().enableParameterTamperingProtection(salt);
+    }
+    
+    @ReactMethod
+    public void setLocation(ReadableArray args){
+        double latitude = Double.parseDouble(args.getString(0));
+        double longitude = Double.parseDouble(args.getString(1));
+        Countly.sharedInstance().setLocation(latitude, longitude);
+    }
+    
+    @ReactMethod
+    public void enableCrashReporting(){
+        Countly.sharedInstance().enableCrashReporting();
+    }
+    
+    @ReactMethod
+    public void addCrashLog(ReadableArray args){
+        String record = args.getString(0);
+        Countly.sharedInstance().addCrashLog(record);
+    }
 
-	 @ReactMethod
-	 public void event(ReadableArray args){
-         String eventType = args.getString(0);
-         if("event".equals(eventType)){
-             String eventName = args.getString(1);
-             int eventCount= Integer.parseInt(args.getString(2));
-             Countly.sharedInstance().recordEvent(eventName, eventCount);
-         }
-         else if ("eventWithSum".equals(eventType)) {
-             String eventName = args.getString(1);
-             int eventCount= Integer.parseInt(args.getString(2));
-             float eventSum= new Float(args.getString(3)).floatValue();
-             Countly.sharedInstance().recordEvent(eventName, eventCount, eventSum);
-         }
-         else if ("eventWithSegment".equals(eventType)) {
-             String eventName = args.getString(1);
-             int eventCount= Integer.parseInt(args.getString(2));
-             HashMap<String, String> segmentation = new HashMap<String, String>();
-             for(int i=3,il=args.size();i<il;i+=2){
-                 segmentation.put(args.getString(i), args.getString(i+1));
-             }
-             Countly.sharedInstance().recordEvent(eventName, segmentation, eventCount);
-         }
-         else if ("eventWithSumSegment".equals(eventType)) {
-             String eventName = args.getString(1);
-             int eventCount= Integer.parseInt(args.getString(2));
-             float eventSum= new Float(args.getString(3)).floatValue();
-             HashMap<String, String> segmentation = new HashMap<String, String>();
-             for(int i=4,il=args.size();i<il;i+=2){
-                 segmentation.put(args.getString(i), args.getString(i+1));
-             }
-             Countly.sharedInstance().recordEvent(eventName, segmentation, eventCount,eventSum);
-         }
-         else{
-         }
-	 }
+   @ReactMethod
+    public void event(ReadableArray args){
+        String eventType = args.getString(0);
+            if("event".equals(eventType)){
+            String eventName = args.getString(1);
+            int eventCount= Integer.parseInt(args.getString(2));
+            Countly.sharedInstance().recordEvent(eventName, eventCount);
+        }
+        else if ("eventWithSum".equals(eventType)) {
+            String eventName = args.getString(1);
+            int eventCount= Integer.parseInt(args.getString(2));
+            float eventSum= new Float(args.getString(3)).floatValue();
+            Countly.sharedInstance().recordEvent(eventName, eventCount, eventSum);
+        }
+        else if ("eventWithSegment".equals(eventType)) {
+            String eventName = args.getString(1);
+            int eventCount= Integer.parseInt(args.getString(2));
+            HashMap<String, String> segmentation = new HashMap<String, String>();
+            for(int i=3,il=args.size();i<il;i+=2){
+            segmentation.put(args.getString(i), args.getString(i+1));
+            }
+            Countly.sharedInstance().recordEvent(eventName, segmentation, eventCount);
+            }
+            else if ("eventWithSumSegment".equals(eventType)) {
+            String eventName = args.getString(1);
+            int eventCount= Integer.parseInt(args.getString(2));
+            float eventSum= new Float(args.getString(3)).floatValue();
+            HashMap<String, String> segmentation = new HashMap<String, String>();
+            for(int i=4,il=args.size();i<il;i+=2){
+            segmentation.put(args.getString(i), args.getString(i+1));
+            }
+            Countly.sharedInstance().recordEvent(eventName, segmentation, eventCount,eventSum);
+        }
+        else{
 
-	 @ReactMethod
-	 public void recordView(ReadableArray args){
-         String viewName = args.getString(0);
-	 	Countly.sharedInstance().recordView(viewName);
-	 }
+        }
+    }
 
-	 @ReactMethod
-	 public void setloggingenabled(ReadableArray args){
-	 	Countly.sharedInstance().setLoggingEnabled(true);
-	 }
+    @ReactMethod
+    public void startEvent(ReadableArray args){
+        String startEvent = args.getString(0);
+        Countly.sharedInstance().startEvent(startEvent);
+    }
 
-	 @ReactMethod
-	 public void setuserdata(ReadableArray args){
-         Map<String, String> bundle = new HashMap<String, String>();
+    @ReactMethod
+    public void endEvent(ReadableArray args){
+        String eventType = args.getString(0);
+        if("event".equals(eventType)){
+            String eventName = args.getString(1);
+            Countly.sharedInstance().endEvent(eventName);
+        }
+        else if ("eventWithSumSegment".equals(eventType)) {
+            String eventName = args.getString(1);
+            int eventCount= Integer.parseInt(args.getString(2));
+            float eventSum= new Float(args.getString(3)).floatValue();
+            HashMap<String, String> segmentation = new HashMap<String, String>();
+            for(int i=4,il=args.length();i<il;i+=2){
+                segmentation.put(args.getString(i), args.getString(i+1));
+            }
+            Countly.sharedInstance().endEvent(eventName, segmentation, eventCount,eventSum);
+        }
+        else{
+        }
+    }
 
-         bundle .put("name", args.getString(0));
-         bundle.put("username", args.getString(1));
-         bundle.put("email", args.getString(2));
-         bundle.put("org", args.getString(3));
-         bundle.put("phone", args.getString(4));
-         bundle.put("picture", args.getString(5));
-         bundle.put("picturePath", args.getString(6));
-         bundle.put("gender", args.getString(7));
-         bundle.put("byear", String.valueOf(args.getInt(8)));
-         Countly.sharedInstance().setUserData(bundle);
-	 }
+	@ReactMethod
+	public void recordView(ReadableArray args){
+        String viewName = args.getString(0);
+		Countly.sharedInstance().recordView(viewName);
+    }
 
-	 @ReactMethod
+	@ReactMethod
+	public void setloggingenabled(){
+		Countly.sharedInstance().setLoggingEnabled(true);
+	}
+
+	@ReactMethod
+	public void setuserdata(ReadableArray args){
+        Map<String, String> bundle = new HashMap<String, String>();
+        bundle .put("name", args.getString(0));
+
+        bundle.put("username", args.getString(1));
+        bundle.put("email", args.getString(2));
+        bundle.put("org", args.getString(3));
+        bundle.put("phone", args.getString(4));
+        bundle.put("picture", args.getString(5));
+        bundle.put("picturePath", args.getString(6));
+        bundle.put("gender", args.getString(7));
+        bundle.put("byear", String.valueOf(args.getInt(8)));
+        Countly.userData.setUserData(bundle);
+        Countly.userData.save();
+	}
+
+	@ReactMethod
 	 public void onregistrationid(ReadableArray args){
-         int messagingMode = Integer.parseInt(args.getString(1));
-         Countly.CountlyMessagingMode mode = null;
-         if(messagingMode == 0){
-             mode = Countly.CountlyMessagingMode.TEST;
-         }
-         else{
-             mode = Countly.CountlyMessagingMode.PRODUCTION;
-         }
-         String projectId = args.getString(2);
-         // Countly.sharedInstance().onRegistrationId(registrationId,mode);
-         Countly.sharedInstance().initMessaging(getCurrentActivity(), getCurrentActivity().getClass(), projectId, mode);
-	 }
+        String registrationId = args.getString(0);
+        int messagingMode = Integer.parseInt(args.getString(1));
+        String projectId = args.getString(2);
+        
+        Countly.CountlyMessagingMode mode = null;
+        if(messagingMode == 0){
+            mode = Countly.CountlyMessagingMode.TEST;
+        }
+        else{
+            mode = Countly.CountlyMessagingMode.PRODUCTION;
+        }
+        Countly.sharedInstance().onRegistrationId(registrationId,mode);
+	}
 
-	 @ReactMethod
-	 public void start(){
-	 	Countly.sharedInstance().onStart(getCurrentActivity());
-	 }
+	@ReactMethod
+	public void start(){
+		Countly.sharedInstance().onStart(getCurrentActivity());
+	}
 
-	 @ReactMethod
-	 public void stop(){
-	 	Countly.sharedInstance().onStop();
-	 }
+	@ReactMethod
+	public void stop(){
+		Countly.sharedInstance().onStop();
+	}
+
+    @ReactMethod
+    public void userData_setProperty(ReadableArray args){
+        String keyName = args.getString(0);
+        String keyValue = args.getString(1);
+        Countly.userData.setProperty(keyName, keyValue);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_increment(ReadableArray args){
+        String keyName = args.getString(0);
+        Countly.userData.increment(keyName);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_incrementBy(ReadableArray args){
+        String keyName = args.getString(0);
+        int keyIncrement = Integer.parseInt(args.getString(1));
+        Countly.userData.incrementBy(keyName, keyIncrement);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_multiply(ReadableArray args){
+        String keyName = args.getString(0);
+        int multiplyValue = Integer.parseInt(args.getString(1));
+        Countly.userData.multiply(keyName, multiplyValue);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_saveMax(ReadableArray args){
+        String keyName = args.getString(0);
+        int maxScore = Integer.parseInt(args.getString(1));
+        Countly.userData.saveMax(keyName, maxScore);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_saveMin(ReadableArray args){
+        String keyName = args.getString(0);
+        int minScore = Integer.parseInt(args.getString(1));
+        Countly.userData.saveMin(keyName, minScore);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_setOnce(ReadableArray args){
+        String keyName = args.getString(0);
+        String minScore = args.getString(1);
+        Countly.userData.setOnce(keyName, minScore);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void userData_setOnce(ReadableArray args){
+        String keyName = args.getString(0);
+        String minScore = args.getString(1);
+        Countly.userData.setOnce(keyName, minScore);
+        Countly.userData.save();
+    }
+
+    @ReactMethod
+    public void recordView(ReadableArray args){
+        String viewName = args.getString(0);
+        Countly.sharedInstance().recordView(viewName);
+    }
 }
+
+
+
+
+
+

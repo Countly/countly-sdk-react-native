e99d70b587e79cda1b94037c160ed9b4aa8cbc38
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2017-04-02 18:55:04 +0530

    js file updated to cordova

--- Countly.js
+++ Countly.js
@@ -18,7 +18,7 @@ Countly.messagingMode = {"TEST":1,"PRODUCTION":0};
 Countly.init = function(serverUrl,appKey){
     Countly.serverUrl = serverUrl;
     Countly.appKey = appKey;
-    CountlyReactNative.init([serverUrl,appKey]);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","init",[serverUrl,appKey]);
 }
 
 Countly.initMessaging = function(options){
@@ -30,7 +30,7 @@ Countly.initMessaging = function(options){
     args.push(options.registrationId || "");
     args.push(options.messageMode || "0");
     args.push(options.projectId || "");
-    CountlyReactNative.onregistrationid(args);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","onregistrationid",args);
 }
 
 // countly sending various types of events
@@ -61,15 +61,15 @@ Countly.sendEvent = function(options){
         args.push(event);
         args.push(segments[event]);
     }
-    CountlyReactNative.event(args);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","event",args);
 }
 Countly.recordView = function(recordView){
-    CountlyReactNative.recordView([recordView || ""]);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","recordView",[recordView || ""]);
 };
 
 // countly enable logger
 Countly.setLoggingEnabled = function(boolean){
-    CountlyReactNative.setloggingenabled([]);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setloggingenabled",[]);
 }
 
 // countly sending user data
@@ -85,7 +85,7 @@ Countly.setUserData = function(options){
     args.push(options.gender || "");
     args.push(options.byear || 0);
 
-    CountlyReactNative.setuserdata(args);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setuserdata",args);
 }
 
 Countly.onRegistrationId = function(options){
@@ -93,16 +93,16 @@ Countly.onRegistrationId = function(options){
     args.push(options.registrationId || "");
     args.push(Countly.messageMode || "0");
     args.push(options.projectId || "");
-    CountlyReactNative.onregistrationid(args);
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","onregistrationid",args);
 }
 // countly start for android
 Countly.start = function(){
-    CountlyReactNative.start();
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","start",[]);
 }
 
 // countly stop for android
 Countly.stop = function(){
-    CountlyReactNative.stop();
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","stop",[]);
 }
 
 // countly deviceready for testing purpose
@@ -122,4 +122,71 @@ Countly.onError = function(error){
 Countly.demo = function(){
 
 }
+
+// 2017
+
+
+Countly.setLocation = function(newDeviceID){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setLocation",[newDeviceID.toString() || ""]);
+}
+Countly.changeDeviceId = function(newDeviceID){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","changeDeviceId",[newDeviceID.toString() || ""]);
+}
+Countly.enableParameterTamperingProtection = function(salt){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","enableParameterTamperingProtection",[salt.toString() || ""]);
+}
+Countly.startEvent = function(eventName){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","startEvent",[eventName.toString() || ""]);
+}
+Countly.endEvent = function(options){
+    if(typeof options === "string")
+        options = {eventName: options};
+    var args = [];
+    var eventType = "event"; //event, eventWithSum, eventWithSegment, eventWithSumSegment
+    var segments = {};
+
+    if(options.segments && options.eventSum)
+        eventType = "eventWithSumSegment";
+
+    args.push(eventType);
+
+    if(options.eventName)
+        args.push(options.eventName.toString());
+    if(options.eventCount)
+        args.push(options.eventCount.toString());
+    if(options.eventSum)
+        args.push(options.eventSum.toString());
+
+    if(options.segments)
+        segments = options.segments;
+    for (var event in segments) {
+        args.push(event);
+        args.push(segments[event]);
+    }
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","endEvent",args);
+};
+
+Countly.userData = {};
+Countly.userData.setProperty = function(keyName, keyValue){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_setProperty",[keyName.toString() || "", keyValue.toString() || ""]);
+};
+Countly.userData.increment = function(keyName){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_increment",[keyName.toString() || ""]);
+};
+Countly.userData.incrementBy = function(keyName, keyIncrement){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_incrementBy",[keyName.toString() || "", keyIncrement.toString() || ""]);
+};
+Countly.userData.multiply = function(keyName, multiplyValue){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_multiply",[keyName.toString() || "", multiplyValue.toString() || ""]);
+};
+Countly.userData.saveMax = function(keyName, saveMax){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_saveMax",[keyName.toString() || "", saveMax.toString() || ""]);
+};
+Countly.userData.saveMin = function(keyName, saveMin){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_saveMin",[keyName.toString() || "", saveMin.toString() || ""]);
+};
+Countly.userData.setOnce = function(keyName, setOnce){
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","userData_setOnce",[keyName.toString() || "", setOnce.toString() || ""]);
+};
+
 export default Countly; 

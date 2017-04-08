3f4f1c96d0c882670ac93409591f7834ae0b5463
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2017-04-08 10:54:56 +0530

    updated changes

--- index.android.js
+++ index.android.js
@@ -1,208 +1,81 @@
-var React = require('react-native');
-var { requireNativeComponent, NativeModules } = React;
+import React, { Component } from 'react';
+import { AppRegistry, Text, Button, View, Image } from 'react-native';
+import Countly from './Countly';
 
-var CountlyReactNative = NativeModules.CountlyReactNative;
+Countly.init("https://try.count.ly","111dcd50d5f4a43a23202330cec19c069a68bc19");
+class AwesomeProject extends Component {
+    constructor(props) {
+        super(props);
+    };    
 
-setInterval(function(){
-    console.log(CountlyReactNative);
-}, 1000);
+    onStart = function(){
+      Countly.start();
+    };
+    onStop = function(){
+      Countly.stop();
+    };
+    onSendUserData = function(){
+      // example for setUserData
+      var options = {};
+      options.name = "Trinisoft Technologies";
+      options.username = "trinisofttechnologies";
+      options.email = "trinisofttechnologies@gmail.com";
+      options.org = "Trinisoft Technologies Pvt. Ltd.";
+      options.phone = "+91 812 840 2946";
+      options.picture = "http://www.trinisofttechnologies.com/images/logo.png";
+      options.picturePath = "";
+      options.gender = "Male";
+      options.byear = 1989;
+      Countly.setUserData(options);
+    };
+    pushMessage = function(){
+      // implementation is pending
 
-var Countly = {};
-Countly.serverUrl = "";
-Countly.appKey = "";
-Countly.ready = false;
-Countly.messagingMode = {"TEST":1,"PRODUCTION":0};
-// countly initialization
-Countly.init = function(serverUrl,appKey){
-    Countly.serverUrl = serverUrl;
-    Countly.appKey = appKey;
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","init",[serverUrl,appKey]);
-}
+    };
+    basicEvent = function(){
+      // example for basic event
+      var events = {"eventName":"basic_event","eventCount":1};
+      Countly.sendEvent(events);
+    };
+    eventWithSum = function(){
+      // example for event with sum
+      var events = {"eventName":"event_sum","eventCount":1,"eventSum":"0.99"};
+      Countly.sendEvent(events);
+    };
+    eventWithSegment = function(){      
+      // example for event with segment
+      var events = {"eventName":"event_segment","eventCount":1};
+      events.segments = {"Country" : "Turkey", "Age" : "28"};
+      Countly.sendEvent(events);
+    };
+    eventWithSumAndSegment = function(){
+      // example for event with segment and sum
+      var events = {"eventName":"event_segment_sum","eventCount":1,"eventSum":"0.99"};
+      events.segments = {"Country" : "Turkey", "Age" : "28"};
+      Countly.sendEvent(events);
+    };
+    allEvent = function(){
 
-Countly.initMessaging = function(options){
-    Countly.projectId = options.projectId;
-    Countly.messageMode = options.messageMode;
-    // Countly.Push.onRegisterPushNotification();
-
-    var args = [];
-    args.push(options.registrationId || "");
-    args.push(options.messageMode || "0");
-    args.push(options.projectId || "");
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","onregistrationid",args);
-}
-
-// countly sending various types of events
-Countly.sendEvent = function(options){
-    var args = [];
-    var eventType = "event"; //event, eventWithSum, eventWithSegment, eventWithSumSegment
-    var segments = {};
-
-    if(options.eventSum)
-        eventType = "eventWithSum";
-    if(options.segments)
-        eventType = "eventWithSegment";
-    if(options.segments && options.eventSum)
-        eventType = "eventWithSumSegment";
-
-    args.push(eventType);
-
-    if(options.eventName)
-        args.push(options.eventName.toString());
-    if(options.eventCount)
-        args.push(options.eventCount.toString());
-    if(options.eventSum)
-        args.push(options.eventSum.toString());
-
-    if(options.segments)
-        segments = options.segments;
-    for (var event in segments) {
-        args.push(event);
-        args.push(segments[event]);
-    }
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","event",args);
-}
-Countly.recordView = function(recordView){
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","recordView",[recordView || ""]);
-};
-
-// countly enable logger
-Countly.setLoggingEnabled = function(boolean){
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setloggingenabled",[]);
-}
-
-// countly sending user data
-Countly.setUserData = function(options){
-    var args = [];
-    args.push(options.name || "");
-    args.push(options.username || "");
-    args.push(options.email || "");
-    args.push(options.org || "");
-    args.push(options.phone || "");
-    args.push(options.picture || "");
-    args.push(options.picturePath || "");
-    args.push(options.gender || "");
-    args.push(options.byear || 0);
-
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setuserdata",args);
-}
-
-Countly.onRegistrationId = function(options){
-    var args = [];
-    args.push(options.registrationId || "");
-    args.push(Countly.messageMode || "0");
-    args.push(options.projectId || "");
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","onregistrationid",args);
-}
-// countly start for android
-Countly.start = function(){
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","start",[]);
-}
-
-// countly stop for android
-Countly.stop = function(){
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","stop",[]);
-}
-
-// countly deviceready for testing purpose
-Countly.deviceready = function(){
-    Countly.ready = true;
-    //testing
-}
-
-// countly dummy success and error event
-Countly.onSuccess = function(result){
-    // alert(result);
-}
-Countly.onError = function(error){
-     // alert("error");
-     // alert(error);
-}
-Countly.demo = function(){
-
-}
-
-/////////////////////////
-////// PUSH CODE ////////
-/////////////////////////
-
-var push = {};
-Countly.Push = push;
-push.onRegisterPushNotification = function(){
-    var pushNotification = window.plugins.pushNotification;
-    if(!window.device){
-        console.error("dependency required org.apache.cordova.device to know android or ios device.");
-        return;
-    }
-    if (device.platform == 'android' || device.platform == 'Android') {
-        pushNotification.register(push.onPushSucess, push.onPushError,{"senderID":Countly.projectId,"ecb":"Countly.Push.onNotificationGCM"});
-    }
-    else{
-        pushNotification.register(push.onPushSucess,push.onPushError,{"badge":"true","sound":"true","alert":"true","ecb":"Countly.Push.onNotificationAPN"});
+    };
+    render() {
+        return (
+          <View>
+            <Text style={[{fontSize:25, textAlign: 'center'}]}>Countly Cordova Demo App</Text>
+            <Image source={{uri: 'https://count.ly/wp-content/uploads/2014/10/countly_logo_color.png'}} style={{width: 300, height: 88}} />
+            < Button onPress = { this.onStart } title = "Start" color = "#5bbd72"> </Button>
+            < Button onPress = { this.onStop } title = "Stop" color = "#d95c5c"> </Button>
+            < Button onPress = { this.onSendUserData } title = "Send Users Data" color = "#00b5ad"> </Button>
+            < Button onPress = { this.pushMessage } title = "Push Message" color = "#00b5ad"> </Button>
+            < Button onPress = { this.basicEvent } title = "Basic Events" color = "#e0e0e0"> </Button>
+            < Button onPress = { this.eventWithSum } title = "Event with Sum" color = "#e0e0e0"> </Button>
+            < Button onPress = { this.eventWithSegment } title = "Event with Segment" color = "#e0e0e0"> </Button>
+            < Button onPress = { this.eventWithSumAndSegment } title = "Even with Sum and Segment" color = "#841584"> </Button>
+            < Button onPress = { this.allEvent } title = "All Event" color = "#1b1c1d"> </Button>
+            < Button onPress = { function(){Countly.recordView("HomePage")} } title = "Record View: 'HomePage'" color = "#e0e0e0"> </Button>
+            < Button onPress = { function(){Countly.recordView("Dashboard")} } title = "Record View: 'Dashboard'" color = "#e0e0e0"> </Button>
+          </View>
+        );
     }
 }
-push.onPushSucess = function(result){
-    push.onPushId(result)
-}
-push.onPushError = function(error){
-    // alert(error)
-}
-push.onPushId = function(pushId){
-    if(pushId == "OK" || pushId == "ok")
-        return
-    push.pushId = pushId;
-    push.onSendPushId();
-}
 
-push.onSendPushId = function(){
-    // alert(push.pushId)
-    var options = {"registrationId":push.pushId,"mode":Countly.messageMode || 0};
-    Countly.onRegistrationId(options);
-}
-push.onSendPushIdCallback = function(err,success){
-    if(err){
-        push.onSendPushId();
-    }
-}
-push.onNotificationGCM = function(e){
-    switch( e.event )
-    {
-        case 'registered':
-            if ( e.regid.length > 0 ){
-                console.log("Regid " + e.regid);
-                push.onPushId(e.regid);
-            }
-        break;
-
-        case 'message':
-
-        break;
-
-        case 'error':
-
-        break;
-
-        default:
-            console.log('An unknown GCM event has occurred');
-        break;
-    }
-}
-push.onNotificationAPN = function(event){
-    var pushNotification = window.plugins.pushNotification;
-        if (event.alert) {
-            navigator.notification.alert(event.alert);
-        }
-        if (event.badge) {
-            pushNotification.setApplicationIconBadgeNumber(push.onPushSucess, push.onPushError, event.badge);
-        }
-        if (event.sound) {
-            var snd = new Media(event.sound);
-            snd.play();
-        }
-}
-
-/////////////////////////
-////// PUSH CODE ////////
-/////////////////////////
-
-
-module.exports = Countly;
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);

97098d3234a22e03a6e9d562d7fdc1968dd451fa
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2019-03-18 14:44:32 +0530

    Removed old test example.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Text, Button, ScrollView, Image, Platform } from 'react-native';
 // import PushNotification from 'react-native-push-notification';
 // import NotificationActions from 'react-native-ios-notification-actions';
 import DeviceInfo from 'react-native-device-info';
-import Countly, { StarRating } from './Countly';
+import Countly, { StarRating } from 'countly-sdk-react-native';
 Countly.enableCrashReporting(true, true);
 
 

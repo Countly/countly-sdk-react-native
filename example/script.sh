a41015183cb362f90a26c7c3d5314f785c1d5e6a
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2019-04-03 20:51:09 +0530

    commit before relocation folders

--- example/script.sh
+++ example/script.sh
@@ -1,10 +1,11 @@
 SDK=/Users/trinisoft/office/countly/app/take6/AwesomeProject/sdk/countly-sdk-react-native
 
 cp ./Countly.js $SDK/Countly.js
-cp ./example/App.js $SDK/../App.js
+cp ./example/App.js $SDK/../../App.js
 
 cp ./android/sdk/src/main/java/ly/count/android/sdk/CountlyReactNative.java $SDK/android/sdk/src/main/java/ly/count/android/sdk/CountlyReactNative.java
 cp ./android/sdk/src/main/java/ly/count/android/sdk/CountlyReactNativePackage.java $SDK/android/sdk/src/main/java/ly/count/android/sdk/CountlyReactNativePackage.java
 
 cp ./ios/CountlyReactNative.h $SDK/ios/CountlyReactNative.h
 cp ./ios/CountlyReactNative.m $SDK/ios/CountlyReactNative.m
+
